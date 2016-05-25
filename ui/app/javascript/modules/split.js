530dc0eaf4cdff01e7bf1aa0f0398b897656c6c9
Josh Parrett <jtparrett@me.com>
2016-05-25 18:04:18 +0100

    split work

--- ui/app/javascript/modules/split.js
+++ ui/app/javascript/modules/split.js
@@ -1,8 +1,12 @@
 BE.modules.split = function(){
   var options = {};
   function init(){
-    options.handle = byBehaviour('handle');
-    
+    byBehaviour('split').on('click', toggle);
+  }
+
+  function toggle(){
+    $('.split__item--shut').removeClass('split__item--shut');
+    $(this).addClass('split__item--open').siblings().removeClass('split__item--open');
   }
 
   return {
